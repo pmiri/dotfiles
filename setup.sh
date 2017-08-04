1c86f919814c92e69967a63d4d52565658336d2e
Matthew Mongrain <matthew.mongrain@gmail.com>
2017-08-04 16:21:31 +0000

    better setup

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo "source ~/.bashrc-extensions\n" | cat - ~/.bashrc > /tmp/out && mv /tmp/out ~/.bashrc
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo apt install fortune cowsay tmux vim
